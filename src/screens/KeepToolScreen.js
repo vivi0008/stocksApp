3d529f669ab6622b1b78adaed1ab3ed1ea55fcda
vivi00008 <70990464+vivi00008@users.noreply.github.com>
2021-03-08 21:41:35 +0700

    update listname, keeptool
    add image

--- src/screens/KeepToolScreen.js
+++ src/screens/KeepToolScreen.js
@@ -1,24 +1,70 @@
-import React from 'react'
+import React from "react";
 import {
     View,
     Text,
     SafeAreaView,
     Image,
     TouchableOpacity,
-    Dimensions
+    Dimensions,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { ScrollView } from "react-native-gesture-handler";
 import styles from "../Style";
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import AntDesign from "react-native-vector-icons/AntDesign";
 
-const KeepToolScreen = ({navigation}) => {
+const KeepToolScreen = ({ navigation }) => {
+    const data = navigation.state.params.data;
 
-    const data = navigation.state.params.data
+    const doBack = () => {
+        navigation.goBack();
+    };
 
-    const doBack = () =>{
-        navigation.goBack()
-    }
+    const getImage = (imgName) => {
+        switch (imgName) {
+            case "helpkeep":
+                return require("../../assets/images/keepTool/helpkeep.jpg");
+                break;
+            case "camera":
+                return require("../../assets/images/keepTool/camera.jpg");
+                break;
+            case "car":
+                return require("../../assets/images/keepTool/car.jpg");
+                break;
+            case "chanwang":
+                return require("../../assets/images/keepTool/chanwang.jpg");
+                break;
+            case "fan":
+                return require("../../assets/images/keepTool/fan.jpg");
+                break;
+            case "helpkeep":
+                return require("../../assets/images/keepTool/helpkeep.jpg");
+                break;
+            case "kaikung":
+                return require("../../assets/images/keepTool/kaikung.jpg");
+                break;
+            case "parate":
+                return require("../../assets/images/keepTool/parate.gif");
+                break;
+            case "roll":
+                return require("../../assets/images/keepTool/roll.jpg");
+                break;
+            case "tarap":
+                return require("../../assets/images/keepTool/tarap.jpg");
+                break;
+            case "tung":
+                return require("../../assets/images/keepTool/tung.jpg");
+                break;
+            case "weight":
+                return require("../../assets/images/keepTool/weight.jpg");
+                break;
+            case "bundai":
+                return require("../../assets/images/keepTool/bundai.jpg");
+                break;
+            default:
+                return require("../../assets/images/shoes.jpg");
+        }
+    };
+
     return (
         <LinearGradient
             colors={["black", "#6e767e", "#ffffff"]}
@@ -27,18 +73,42 @@ const KeepToolScreen = ({navigation}) => {
             <SafeAreaView style={styles.root}>
                 <View style={styles.container}>
                     <TouchableOpacity onPress={() => doBack()}>
-                    <AntDesign name="arrowleft" size={32} color="white"/>
+                        <AntDesign name="arrowleft" size={32} color="white" />
                     </TouchableOpacity>
-                    <Text style={[styles.textHeader, {fontSize:24, marginTop:5}]}>{data.title}</Text>
+                    <Text
+                        style={[
+                            styles.textHeader,
+                            { fontSize: 24, marginTop: 5 },
+                        ]}
+                    >
+                        {data.title}
+                    </Text>
                 </View>
                 <ScrollView style={{ flex: 1 }}>
-                    <View style={[styles.contentBg, {borderBottomLeftRadius : 12, borderBottomRightRadius:12}]}>
-                        <Text style={styles.textContent}>{"\t"}{data.detail}</Text>
+                    <View
+                        style={[
+                            styles.contentBg,
+                            {
+                                borderBottomLeftRadius: 12,
+                                borderBottomRightRadius: 12,
+                            },
+                        ]}
+                    >
+                        <Text style={styles.textContent}>
+                            {"\t"}
+                            {data.detail}
+                        </Text>
+                        <Image
+                            source={getImage(data.image)}
+                            resizeMethod="auto"
+                            resizeMode="stretch"
+                            style={styles.imgStock}
+                        />
                     </View>
                 </ScrollView>
             </SafeAreaView>
         </LinearGradient>
-    )
-}
+    );
+};
 
-export default KeepToolScreen
+export default KeepToolScreen;
--- src/screens/ListNameScreen.js
+++ src/screens/ListNameScreen.js
@@ -19,6 +19,46 @@ const ListNameScreen = ({ navigation }) => {
         navigation.goBack();
     };
 
+    const getImage = (imgName) => {
+        switch (imgName) {
+            case "747":
+                return require("../../assets/images/place/747.jpg");
+                break;
+            case "am":
+                return require("../../assets/images/place/am.jpg");
+                break;
+            case "aommata":
+                return require("../../assets/images/place/aommata.jpg");
+                break;
+            case "berapat":
+                return require("../../assets/images/place/berapat.jpg");
+                break;
+            case "chok":
+                return require("../../assets/images/place/chok.gif");
+                break;
+            case "infinity":
+                return require("../../assets/images/place/infinity.jpg");
+                break;
+            case "kaitak":
+                return require("../../assets/images/place/kaitak.jpg");
+                break;
+            case "koduk":
+                return require("../../assets/images/place/koduk.jpg");
+                break;
+            case "proin":
+                return require("../../assets/images/place/proin.jpg");
+                break;
+            case "tepa":
+                return require("../../assets/images/place/tepa.jpg");
+                break;
+            case "tip":
+                return require("../../assets/images/place/tip.jpg");
+                break;
+            default:
+                return require("../../assets/images/shoes.jpg");
+        }
+    };
+
     return (
         <LinearGradient
             colors={["black", "#6e767e", "#ffffff"]}
@@ -42,7 +82,10 @@ const ListNameScreen = ({ navigation }) => {
                     <View style={{ marginHorizontal: 20 }}>
                         {placeData.map((e, index) => {
                             return (
-                                <Card style={{ marginVertical: 10 }} key={index}>
+                                <Card
+                                    style={{ marginVertical: 10 }}
+                                    key={index}
+                                >
                                     <Card.Content
                                         style={{
                                             backgroundColor: "black",
@@ -54,7 +97,7 @@ const ListNameScreen = ({ navigation }) => {
                                                 fontFamily: "kanitRegular",
                                                 fontSize: 18,
                                                 textAlign: "center",
-                                                color:'white'
+                                                color: "white",
                                             }}
                                         >
                                             {e.name}
@@ -82,11 +125,17 @@ const ListNameScreen = ({ navigation }) => {
                                         <Text
                                             style={{
                                                 fontFamily: "kanitRegular",
-                                                marginTop: 5,
+                                                marginVertical:10
                                             }}
                                         >
                                             website : {e.website}
                                         </Text>
+                                        <Image
+                                            source={getImage(e.image)}
+                                            resizeMethod="auto"
+                                            resizeMode="stretch"
+                                            style={styles.imgStock}
+                                        />
                                     </Card.Content>
                                 </Card>
                             );
